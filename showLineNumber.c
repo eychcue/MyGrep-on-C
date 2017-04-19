e6da3be65d1c7c799575739234e90259e4f62510
Emmanuel Hmar <EmmanuelHmar@users.noreply.github.com>
2017-04-19 13:47:11 -0400

    Update showLineNumber.c
    Added show String with the Line number function

--- showLineNumber.c
+++ showLineNumber.c
@@ -1,15 +1,35 @@
-void showLineNumber(FILE* f) {
+void showStringAndLineNumber(char* str, FILE* fp) {
+    int Max_Char = 256;//Set the maximum character to 256
+    char line[Max_Char];//Create a character array with limit of 256
     int lineNum = 1;//Start the line number from 1
+
+    /**fgets scan the file in one shot
+    ** @line is where to save the line
+    ** @Max_Char is the maximum size of this string
+    ** @ fp is the file source
+    **/
+    while (fgets(line, Max_Char, fp)!= NULL) {
+        
+        //ststr checks str(haystack) if it contains line (needle)
+        if (strstr(line,str) != NULL) {
+            printf("%d: %s", lineNum++, line);//If it not Null, then print that line with the line number
+        }
+    }
+}
+/* print file content with line number */
+void showLineNumber(FILE* fp) {
+    int lineNum = 1;//Start the line number with 1
     char c;//Store a character from file
 
     //While it is not the end of file
-    while ((c = fgetc(f)) != EOF) {
+    while ((c = getc(fp)) != EOF) {
         //print out the line number inside brackets
-        printf("[%d]: ", lineNum++);
-        
-        //Loop through the characters line by line and print it
-        for ( c ; c!='\n' ; c=getc(f)) {
+        printf("%d: ", lineNum++);
+
+        //Loop through the characters line by line
+        while (c!='\n') {
             printf("%c", c);
+	    c = getc(fp);
         }
         printf("\n");//Prints a new line 
     }
